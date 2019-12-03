3801417bffbbab82ba2f8f81cc11f31c16f7d17c
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-12-03 21:56:47 +0200

    Create Customer Validator

---
+++ WebApp/WebApp/Infrastructure/CustomerUserValidator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Infrastructure
+{
+    public class CustomerUserValidator : IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            if (user.Email.ToLower().EndsWith("@outlook.com"))
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError
+                {
+                    Code = "Email DomainError",
+                    Description = "Only example.com email addresses allowed",
+                }));
+            }
+        }
+    }
+}
