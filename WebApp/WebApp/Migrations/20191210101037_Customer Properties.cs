3ba8c25011579bc01bb2a58f54efa061e426ac14
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-12-10 12:11:27 +0200

    Add Migrations

---
+++ WebApp/WebApp/Migrations/20191210101037_Customer Properties.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class CustomerProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Cities",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "QualitificationLevel",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cities",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "QualitificationLevel",
+                table: "AspNetUsers");
+        }
+    }
+}
