2e6153bd55e355f37b1388433fe8ddb78aecb811
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-12-10 12:07:38 +0200

    Add User Props and create View

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -4,22 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApp.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,UserManager<AppUser> userMgr)
         {
             _logger = logger;
-        }
-
-       
+            _userManager = userMgr;
+        }       
 
         public IActionResult Privacy()
         {
@@ -42,7 +44,34 @@ namespace WebApp.Controllers
             ["User"] = HttpContext.User.Identity.Name,
             ["Authenticated"] = HttpContext.User.Identity.IsAuthenticated,
             ["Auth Type"] = HttpContext.User.Identity.AuthenticationType,
-            ["In Users Role"] = HttpContext.User.IsInRole("Users")
+            ["In Users Role"] = HttpContext.User.IsInRole("Users"),
+            ["City"] = CurrentUser.Result.Cities,
+            ["Qualitification"] = CurrentUser.Result.QualitificationLevel
         };
+        [Authorize]
+        public async Task<IActionResult>UserProps()
+        {
+           
+            return View(await CurrentUser);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> UserProps([Required]Cities cities,[Required] QualitificationLevel qualitificationLevel)
+        {
+            if(ModelState.IsValid)
+            {
+                AppUser user = await CurrentUser;
+                user.Cities = cities;
+                user.QualitificationLevel = qualitificationLevel;
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index");
+            }
+            return View(await CurrentUser);
+        }
+
+        private Task<AppUser>CurrentUser=> _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
+
+
+
     }
 }
