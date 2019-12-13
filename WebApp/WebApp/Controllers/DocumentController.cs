f8e5f0b51e5417bec8d6edb6215e84ae5a84f87e
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-12-13 20:22:48 +0200

    Use a policy to authorize access to resources

---
+++ WebApp/WebApp/Controllers/DocumentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Authorize]
+    public class DocumentController : Controller
+    {
+        private ProtectedDocument[] docs = new ProtectedDocument[]
+        {
+            new ProtectedDocument{Title ="Q3 Budget",Author="Vasyl", Editor="Nastya"},
+            new ProtectedDocument{Title="Project Plan",Author = "Bob",Editor="Vasyl"}
+        };
+
+        public ViewResult Index()
+        {
+            return View(docs);
+        }
+
+        public ViewResult Edit(string title)
+        {
+            return View("Index", docs.FirstOrDefault(d => d.Title == title));
+        }
+    }
+}
